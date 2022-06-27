9496e0d728cd5cba101da9cbecaf6cc47d204f2e
boonaki <joshdurr17@gmail.com>
2022-06-27 19:26:08 -0400

    formatting and data

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use('/assets', express.static('assets'));
 const PORT = process.env.PORT || 8000
 
 /*
-Each tea will have name, image, origin, type (if not base type), caffeine levels, description, taste and color
+Each tea will have name, any alternate names, image, origin, type (if not base type), caffeine levels, description, taste and color
 NOTE: not every tea known to man is in this database
 If tea is a blend, include disclaimer that original type is only included in the blend
 https://tea-api-boonaki.herokuapp.com/
@@ -52,13 +52,13 @@ const teas = {
 
                 'decription' : 'Darjeeling tea is a black tea that is grown and processed in the Darjeeling or Kalimpong Districts in West Bengal, also among the only teas in the world with a Geographical Indication trademark',
 
-                'sources' : ['hhttps://en.wikipedia.org/wiki/Darjeeling_tea', 'https://www.thespruceeats.com/tea-flushes-in-darjeeling-765194', 'https://www.seriouseats.com/why-you-should-drink-more-darjeeling-tea-what-is-first-flush'],
+                'sources' : ['https://en.wikipedia.org/wiki/Darjeeling_tea', 'https://www.thespruceeats.com/tea-flushes-in-darjeeling-765194', 'https://www.seriouseats.com/why-you-should-drink-more-darjeeling-tea-what-is-first-flush'],
 
-                'colorDescription' : 'varying based on how its brewed, from light yellow to orange to a deep red.',
+                'colorDescription' : 'varying based on how its brewed, from a golden yellow to orange to a deep red.',
                 'tasteDescription' : 'musky-sweet tasting notes similar to muscat wine',
             },
-            
-            "lapsangSouchong" : {
+
+            "lapsangsouchong" : {
                 'name' : 'Lapsang Souchong',
                 'altnames' : 'Smoked Tea, Smoky Souchong',
                 'image' : 'https://commons.wikimedia.org/wiki/File:Lapsang_Souchong.jpg#/media/File:Lapsang_Souchong.jpg',
@@ -90,106 +90,188 @@ const teas = {
                 'tasteDescription' : 'smooth, fruity, malty, unsweetened cocoa',
             },
 
-            "dianHong" : {
-                'name' : 'Dian Hong',
-                'altnames' : '',
-                'image' : '',
+            "dianhong" : {
+                'name' : 'Dianhong Tea',
+                'altnames' : 'Yunnan Red Tea',
+                'image' : 'https://commons.wikimedia.org/wiki/File:GoldenDianHong.jpg#/media/File:GoldenDianHong.jpg',
 
-                'origin' : '',
-                'caffeine' : '',
+                'origin' : 'Yunnan Province, China',
+                'caffeine' : '20-40mg',
 
-                'decription' : '',
+                'decription' : 'A relatively high-end, gourmet Chinese black tea sometimes used in various tea blends. The main difference between Dianhong and other Chinese black teas is the amount of fine leaf buds, or "golden tips," present in the dried tea.',
 
-                'sources' : [''],
+                'sources' : ['https://en.wikipedia.org/wiki/Dianhong'],
 
-                'colorDescription' : '',
-                'tasteDescription' : '',
-            },
-            "orangePekoe" : {
-                'name' : 'Orange Pekoe',
-                'altnames' : '',
-            },
-        },
-        'blends' : {
-            "earlGrey" : {
-                'name' : 'Earl Grey',
-                'altnames' : '',
-            },
-            "ladyGrey" : {
-                'name' : 'Lady Grey',
-                'altnames' : '',
-            },
-            "englishBreakfast" : {
-                'name' : 'English Breakfast',
-                'altnames' : '',
-            },
-            "englishAfternoon" : {
-                'name' : 'English Afternoon',
-                'altnames' : '',
-            },
-            "irishBreakfast" : {
-                'name' : 'Irish Breakfast',
-                'altnames' : '',
-            },
-            "masalaChai" : {
-                'name' : 'Masala Chai',
-                'altnames' : '',
-            },
-            "russianCaravan" : {
-                'name' : 'Russian Caravan',
-                'altnames' : '',
-            },
-            "lycheeBlackTea" : {
-                'name' : 'Lychee Black Tea',
-                'altnames' : '',
+                'colorDescription' : 'golden orange',
+                'tasteDescription' : 'sweet, floral, and honey-like, though tends to be stronger and more bitter than other red teas',
             },
         }
     },
+
+    /*
+        'name' : '',
+        'altnames' : '',
+        'image' : '',
+                
+        'origin' : '',
+        'caffeine' : '',
+
+        'description' : '',
+        'colorDescription' : '',
+        'tasteDescription' : '',
+
+        'sources' : [''],
+    */
 
     'green' : {
         'name': 'Green Tea',
         'image' : 'https://tea-api-boonaki.herokuapp.com/assets/green.jpg',
         'origin' : 'China',
         'caffeine' : '~10mg',
-        'description' : '',
+        'sources' : ['https://en.wikipedia.org/wiki/Green_tea'],
+        'description' : 'Made from unoxidized leaves, Green tea is one of the least processed types of tea, offering high levels of antioxidants.',
         'colorDescription' : 'varying shades of green',
         'tasteDescription' : 'sweet, bittersweet, nutty, vegetal, buttery, floral, fruity, oceanic',
-        'blends' : {
-            "Sencha" : {
+        'types' : {
+            "sencha" : {
+                'name' : 'Sencha Tea',
+                'altnames' : 'Steeped Tea',
+                'image' : 'https://commons.wikimedia.org/wiki/File:2017_Kagoshima_sencha.jpg#/media/File:2017_Kagoshima_sencha.jpg',
+                
+                'origin' : 'Japan',
+                'caffeine' : '20-40mg',
 
+                'description' : 'A Japanese green tea prepared by infusing whole tea leaves in hot water.',
+                'colorDescription' : 'Light green',
+                'tasteDescription' : 'fresh, herbal, or grassy flavor, which can have varying vegetal grassy notes depending on how long it is steeped.',
+
+                'sources' : ['https://en.wikipedia.org/wiki/Sencha'],
             },
-            "Matcha" : {
+            "matcha" : {
+                'name' : 'Matcha Tea',
+                'altnames' : '',
+                'image' : 'https://commons.wikimedia.org/wiki/File:Matcha_Scoop.jpg#/media/File:Matcha_Scoop.jpg',
+                
+                'origin' : 'China',
+                'caffeine' : '20-90mg',
 
+                'description' : 'A finely ground powder of specially grown and processed green tea leaves, traditionally consumed in East Asia.',
+                'colorDescription' : 'Green, dark green',
+                'tasteDescription' : 'vegetal grassy notes, natural sweet nuttiness, a touch of bitterness with a pleasant savory ending',
+
+                'sources' : ['https://en.wikipedia.org/wiki/Matcha'],
             },
-            "Shincha" : {
+            "bancha" : {
+                'name' : 'Bancha',
+                'altnames' : '',
+                'image' : 'https://commons.wikimedia.org/wiki/File:Shizuoka_bancha.jpg#/media/File:Shizuoka_bancha.jpg',
+                
+                'origin' : 'Japan',
+                'caffeine' : '~10mg',
 
+                'description' : 'Tea that is harvested from the same tea tree as sencha grade, but it is plucked later than sencha is. Can be found in a number of forms such as roasted, unroasted, smoked, etc.',
+                'colorDescription' : 'Light green to green-orange',
+                'tasteDescription' : 'mild, earthy grassiness, with dry, toasty notes with less of a deeply vegetal, umami flavo',
+
+                'sources' : ['https://en.wikipedia.org/wiki/Bancha'],
             },
-            "Bancha" : {
+            "gyokuro" : {
+                'name' : 'Gyokuro',
+                'altnames' : 'Jade Dew, Pearl Dew',
+                'image' : 'https://commons.wikimedia.org/wiki/File:Gyokuro_img_0067.jpg#/media/File:Gyokuro_img_0067.jpg',
+                
+                'origin' : 'Japan',
+                'caffeine' : '~35mg',
 
+                'description' : 'A type of shaded green tea from Japan. It differs from the standard sencha (a classic unshaded green tea) in being grown under the shade rather than the full sun.',
+                'colorDescription' : 'ranging from light green to dark green',
+                'tasteDescription' : 'very full-bodied, with a taste reminiscent to seaweed and grasses, followed by an intense sweetness',
+
+                'sources' : ['https://en.wikipedia.org/wiki/Gyokuro'],
             },
-            "Gyokuro" : {
+            "kukicha" : {
+                'name' : 'Kukicha',
+                'altnames' : 'Bōcha, Twig tea, Stalk tea',
+                'image' : 'https://commons.wikimedia.org/wiki/File:Kukicha.jpg#/media/File:Kukicha.jpg',
+                
+                'origin' : 'Japan',
+                'caffeine' : '1-5mg',
 
+                'description' : 'A Japanese blend made of stems, stalks, and twigs. It is available as a green tea or in more oxidised processing',
+                'colorDescription' : 'very pale, yellow-green,',
+                'tasteDescription' : 'mildly nutty and slightly creamy sweet flavour',
+
+                'sources' : ['https://en.wikipedia.org/wiki/Kukicha'],
             },
-            "Kukicha" : {
+            "konacha" : {
+                'name' : 'Konacha',
+                'altnames' : 'Gyokurokonacha',
+                'image' : 'https://commons.wikimedia.org/wiki/File:Konacha.jpg#/media/File:Konacha.jpg',
+                
+                'origin' : 'Japan',
+                'caffeine' : 'varied based on plant growth',
 
+                'description' : 'A type of green tea, composed of the dust, tea buds and small leaves that are left behind after processing Gyokuro or Sencha',
+                'colorDescription' : 'intense green, deep green',
+                'tasteDescription' : 'mild, sweet, and grassy flavor with bitter after taste',
+
+                'sources' : ['https://en.wikipedia.org/wiki/Konacha'],
             },
-            "Tencha" : {
+            "genmaicha" : {
+                'name' : '',
+                'altnames' : '',
+                'image' : '',
+                
+                'origin' : '',
+                'caffeine' : '',
 
+                'description' : '',
+                'colorDescription' : '',
+                'tasteDescription' : '',
+
+                'sources' : [''],
             },
-            "Konacha" : {
+            "hojicha" : {
+                'name' : '',
+                'altnames' : '',
+                'image' : '',
+                
+                'origin' : '',
+                'caffeine' : '',
 
+                'description' : '',
+                'colorDescription' : '',
+                'tasteDescription' : '',
+
+                'sources' : [''],
             },
-            "Genmaicha" : {
+            "fukamushi Sencha" : {
+                'name' : '',
+                'altnames' : '',
+                'image' : '',
+                
+                'origin' : '',
+                'caffeine' : '',
 
+                'description' : '',
+                'colorDescription' : '',
+                'tasteDescription' : '',
+
+                'sources' : [''],
             },
-            "Hojicha" : {
+            "kabusecha" : {
+                'name' : '',
+                'altnames' : '',
+                'image' : '',
+                
+                'origin' : '',
+                'caffeine' : '',
 
-            },
-            "Fukamushi Sencha" : {
+                'description' : '',
+                'colorDescription' : '',
+                'tasteDescription' : '',
 
-            },
-            "Kabusecha" : {
-
+                'sources' : [''],
             }
         }
     },
@@ -202,13 +284,13 @@ const teas = {
         'colorDescription' : 'bright green or yellow, or dark amber and red',
         'tasteDescription' : 'Similar to black tea, though varied oxidation levels results in widely varied tastes',
         'types' : {
-            "Da Hong Pao" : {
+            "dahongpao" : {
 
             },
-            "Shui Jin Gui" : {
+            "shuijingui" : {
 
             },
-            "Tieluohan" : {
+            "tieluohan" : {
 
             },
             "Bai Jiguan" : {
@@ -310,17 +392,112 @@ const teas = {
         }
     },
 
-    'yellow' : {
-        'name' : 'Yellow Tea',
-        'caffeine' : 'time/temperature based',
-        'origin' : 'East Asia',
-        'description' : '',
-        'colorDescription' : 'Light green or light yellow',
-        'tasteDescription' : 'floral, fresh, fruity, with a hint of cucumber or melon.',
-        'types' : {
+    //HERBAL BLENDS - BLENDS WILL CONTAIN ALL KEYS + INGREDIENTS
+    'blends' : {
+        "earlgrey" : {
+            'name' : 'Earl Grey',
+            'altnames' : '',
+            'image' : 'https://commons.wikimedia.org/wiki/File:Earl_Grey_Tea.jpg#/media/File:Earl_Grey_Tea.jpg',
+            
+            'origin' : 'UK*',
+            'caffeine' : '40-120',
+            'mainIngredients' : 'Black Tea, Oil of Bergamot',
+
+            'description' : 'A tea blend which has been flavoured with oil of bergamot',
+            'colorDescription' : 'Amber, deep orange',
+            'tasteDescription' : 'smoky, earthy, spicy, nutty, citrus, caramel, leather, fruity, and honey',
+
+            'sources' : ['https://en.wikipedia.org/wiki/Earl_Grey_tea'],
+        },
+
+        "ladygrey" : {
+            'name' : 'Lady Grey',
+            'altnames' : '',
+            'image' : 'https://commons.wikimedia.org/wiki/File:TwiningsLadyGrey_low.jpg#/media/File:TwiningsLadyGrey_low.jpg',
+            
+            'origin' : 'UK*',
+            'caffeine' : '25-110mg',
+            'mainIngredients' : 'Black Tea, Oil of Bergamot, Lemon peel, Orange Peel',
+    
+            'description' : 'Lady Grey tea is a trademarked variation on Earl Grey tea. Like Earl Grey, it is a black tea flavoured with bergamot essential oil.',
+            'colorDescription' : 'lighter amber, orange',
+            'tasteDescription' : '',
+    
+            'sources' : ['https://en.wikipedia.org/wiki/Lady_Grey_(tea)'],
+        },
+        "englishbreakfast" : {
+            'name' : 'English Breakfast',
+            'altnames' : '',
+        },
+        "englishafternoon" : {
+            'name' : 'English Afternoon',
+            'altnames' : '',
+        },
+        "irishbreakfast" : {
+            'name' : 'Irish Breakfast',
+            'altnames' : '',
+        },
+        "masalachai" : {
+            'name' : 'Masala Chai',
+            'altnames' : '',
+        },
+        "russiancaravan" : {
+            'name' : 'Russian Caravan',
+            'altnames' : '',
+        },
+        "lycheeblackTea" : {
+            'name' : 'Lychee Black Tea',
+            'altnames' : '',
+        },
+        "sencha" : {
 
         },
+        "matcha" : {
+
+        },
+        "shincha" : {
+
+        },
+        "bancha" : {
+
+        },
+        "gyokuro" : {
+
+        },
+        "kukicha" : {
+
+        },
+        "tencha" : {
+
+        },
+        "konacha" : {
+
+        },
+        "genmaicha" : {
+
+        },
+        "hojicha" : {
+
+        },
+        "fukamushi Sencha" : {
+
+        },
+        "kabusecha" : {
+
+        }
     },
+
+    // 'yellow' : {
+    //     'name' : 'Yellow Tea',
+    //     'caffeine' : 'time/temperature based',
+    //     'origin' : 'East Asia',
+    //     'description' : '',
+    //     'colorDescription' : 'Light green or light yellow',
+    //     'tasteDescription' : 'floral, fresh, fruity, with a hint of cucumber or melon.',
+    //     'types' : {
+
+    //     },
+    // },
 
     'unknown' : {
         'Origin' : 'NA',
@@ -344,6 +521,7 @@ app.get('/api/teas/:name', (req,res) => {
     }else{
         res.json(teas.unknown)
     }
+    // TODO: find tea type if not one of the main types and return tea in types key of main type(black,white,oolong,green)
 })
 
 
