3e61639a3abafbe523d3795009df218fd1508d89
boonaki <joshdurr17@gmail.com>
2022-09-02 11:51:35 -0400

    added style changes

--- public/main.js
+++ public/main.js
@@ -5,3 +5,13 @@ let animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'https://assets1.lottiefiles.com/packages/lf20_7zfgb7k9.json' // lottie file path
 })
+
+function openReq(id){
+    id = id.split('-')[1]
+    let expandeds = document.getElementsByClassName('res')
+    console.log(expandeds.length + 1, id)
+    for(let i = 1; i < expandeds.length + 1; i++){
+        expandeds[i].classList.add('hidden')
+    }
+    document.getElementById(`res-${id}`).classList.remove('hidden')
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,16 @@ module.exports = {
   content: ["./public/index.html", ".public/main.js"],
   theme: {
     extend: {
-        // color: {
-        //     slateBlue : '#8599B6',
-        //     darkerBlue : '#1E293B',
-        //     lightWhite : '#CBD5E1',
-        //     whiter : '#F8FAFC',
-        // },
+        colors: {
+            'background' : '#302A38',
+            'spanTan' : 'rgb(251 235 217)',
+            'grey': '#bdc8d1',
+            'someSlate': '#748b9f'
+        },
+        margin: {
+            '1/5': '20%',
+            '3/20': '15%'
+        }
     },
   },
   plugins: [],
